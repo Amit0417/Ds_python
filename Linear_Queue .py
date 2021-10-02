c8d31cbddc778d89cec078e5d745078ef11cf069
AMIT KUMAR MAHOUR <akumarrockstar@gmail.com>
2021-10-01 22:56:46 -0700

    Update and rename L_Queue .py to Linear_Queue .py

--- Linear_Queue .py
+++ Linear_Queue .py
@@ -1,9 +1,13 @@
-def enqueue(a):
+#linear Queue 
+#linear Queue uses FIFO(first in first out )method
+
+
+def enqueue(a): # for adding element in list
     element = int(input("Enter a number"))
     a.append(element)
 
 
-def dequeue(a):
+def dequeue(a): # for poping element in list
     if len(a) == 0:
         print("Queue is underflow")
     else:
@@ -11,7 +15,7 @@ def dequeue(a):
         print("dequeue number is :", element)
 
 
-def peak(a):
+def peak(a): # for displaying the peak element in list
     if len(a) == 0:
         print("Queue is underflow")
     else:
@@ -19,7 +23,7 @@ def peak(a):
         print("Peak element is :", element)
 
 
-def display(a):
+def display(a): # for displaying whole list 
     if len(a) == 0:
         print("Queue is underflow")
     else:
@@ -29,7 +33,7 @@ def display(a):
 a = []
 while True:
     choice = int(input(
-        "press 1 for enqueue \npress 2 for dequeue\npress 3 for peak element\npress 4 for display list\npress any other keys exit "))
+        "press 1 for enqueue \npress 2 for dequeue\npress 3 for peak element\npress 4 for display list\npress any other keys exit ")) # making choices for calling the functions
     if choice == 1:
         enqueue(a)
     elif choice == 2:
@@ -38,5 +42,5 @@ while True:
         peak(a)
     elif choice == 4:
         display(a)
-    else:
-        break
+    else: 
+        break # for exit the program
